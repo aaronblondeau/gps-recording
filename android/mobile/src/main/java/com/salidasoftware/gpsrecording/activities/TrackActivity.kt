ff98fcf739cff9c0582b0a47e04096b9caed789d
Aaron Blondeau <aaronblondeau@gmail.com>
2018-09-23 15:51:53 -0600

    Make track information editable

--- android/mobile/src/main/java/com/salidasoftware/gpsrecording/activities/TrackActivity.kt
+++ android/mobile/src/main/java/com/salidasoftware/gpsrecording/activities/TrackActivity.kt
@@ -9,12 +9,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import com.salidasoftware.gpsrecording.GPSRecordingApplication
 import com.salidasoftware.gpsrecording.R
 import com.salidasoftware.gpsrecording.room.Track
 import com.salidasoftware.gpsrecording.view_models.TrackViewModel
 import com.salidasoftware.gpsrecording.databinding.ActivityTrackBinding
 import kotlinx.android.synthetic.main.activity_track.*
+import kotlinx.android.synthetic.main.content_track.*
 import org.jetbrains.anko.*
 import java.io.IOException
 
@@ -85,15 +87,56 @@ class TrackActivity : AppCompatActivity() {
         }
 
         // Fetch the track and add it to the view model
+        buttonSaveTrack.isEnabled = false
         val trackId = intent.getLongExtra(TRACK_ID, -1)
         if (trackId >= 0 && store != null) {
             doAsync {
                 track = store.trackDAO.getByIdLive(trackId)
                 trackViewModel.setTrack(this@TrackActivity, track)
+                buttonSaveTrack.isEnabled = true
             }
         } else {
             this.finish()
         }
+
+        buttonSaveTrack.setOnClickListener {
+            Log.d("TrackActivity", "~~ " + trackDetailName.text.toString() + " | " + trackDetailNote.text.toString())
+            buttonSaveTrack.isEnabled = false
+            doAsync {
+                store?.let { store ->
+                    this@TrackActivity.track.value?.let {
+
+                        var activity = ""
+                        if (radioButtonBike.isChecked) {
+                            activity = Track.Activity.BIKE.activityName
+                        }
+                        if (radioButtonRun.isChecked) {
+                            activity = Track.Activity.RUN.activityName
+                        }
+                        if (radioButtonSki.isChecked) {
+                            activity = Track.Activity.SKI.activityName
+                        }
+                        if (radioButtonHike.isChecked) {
+                            activity = Track.Activity.HIKE.activityName
+                        }
+                        if (radioButtonWalk.isChecked) {
+                            activity = Track.Activity.WALK.activityName
+                        }
+
+                        store.updateTrack(it, trackDetailName.text.toString(), trackDetailNote.text.toString(), activity)
+                        uiThread {
+                            toast(getString(R.string.track_saved))
+                        }
+                    }
+                }
+                uiThread {
+                    buttonSaveTrack.isEnabled = true
+                }
+            }
+            trackDetailName.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            trackDetailNote.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean
--- android/mobile/src/main/java/com/salidasoftware/gpsrecording/view_models/TrackViewModel.kt
+++ android/mobile/src/main/java/com/salidasoftware/gpsrecording/view_models/TrackViewModel.kt
@@ -13,8 +13,16 @@ class TrackViewModel() : ViewModel() {
 
     // https://developer.android.com/reference/android/databinding/ObservableField
     val name : ObservableField<String> = ObservableField("")
+    val note : ObservableField<String> = ObservableField("")
     val distance : ObservableField<String> = ObservableField("")
     val duration : ObservableField<String> = ObservableField("")
+    val activity : ObservableField<String> = ObservableField("")
+
+    val isHike: ObservableField<Boolean> = ObservableField(false)
+    val isBike: ObservableField<Boolean> = ObservableField(false)
+    val isRun: ObservableField<Boolean> = ObservableField(false)
+    val isSki: ObservableField<Boolean> = ObservableField(false)
+    val isWalk: ObservableField<Boolean> = ObservableField(false)
 
     var observer: Observer<Track>? = null
     var observed: LiveData<Track>? = null
@@ -22,9 +30,24 @@ class TrackViewModel() : ViewModel() {
     fun unsetTrack() {
         unObserve()
         observed = null
+        clearObservables()
+    }
+
+    fun clearObservables() {
         name.set("")
+        note.set("")
+        activity.set("")
         distance.set("")
         duration.set("")
+        clearActivityObservables()
+    }
+
+    fun clearActivityObservables() {
+        isHike.set(false)
+        isBike.set(false)
+        isRun.set(false)
+        isSki.set(false)
+        isWalk.set(false)
     }
 
     fun setTrack(owner: LifecycleOwner, track: LiveData<Track>) {
@@ -35,14 +58,32 @@ class TrackViewModel() : ViewModel() {
         observer = Observer {
             Log.d("TrackActivity", "~~ Track LiveData changed!")
             if (it == null) {
-                name.set("")
-                distance.set("")
-                duration.set("")
+                clearObservables()
                 unObserve()
             } else {
                 name.set(it.name)
+                note.set(it.note)
+                activity.set(it.activity)
                 distance.set(track.value?.formattedDistance(GPSRecordingStore.displayMetricUnits.get() ?: false))
                 duration.set(track.value?.formattedDuration())
+
+                clearActivityObservables()
+                if(activity.get().equals(Track.Activity.RUN.activityName)) {
+                    isRun.set(true)
+                }
+                if(activity.get().equals(Track.Activity.HIKE.activityName)) {
+                    isHike.set(true)
+                }
+                if(activity.get().equals(Track.Activity.BIKE.activityName)) {
+                    isBike.set(true)
+                }
+                if(activity.get().equals(Track.Activity.SKI.activityName)) {
+                    isSki.set(true)
+                }
+                if(activity.get().equals(Track.Activity.WALK.activityName)) {
+                    isWalk.set(true)
+                }
+
             }
         }
         track.observe(owner, observer!!)
